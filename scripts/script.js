efafc79197c8bc7faf15ae1b44793f0fd5706687
Victor L <le.victor@hotmail.com>
2022-07-07 09:42:31 -0500

    adding slider nav functionality

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+const sliderContainer = document.querySelector('.slider__container');
+const sliderLeftBtn = document.querySelector('.nav__button__left');
+const sliderRightBtn = document.querySelector('.nav__button__right');
+
+var x = 0;
+
+
+sliderLeftBtn.addEventListener('click', () => {
+  if (x < 0) {
+    x += 1200;
+    sliderContainer.setAttribute('style', `left: ${x}px`);
+    console.log(x);
+  } else { return }
+});
+
+sliderRightBtn.addEventListener('click', () => {
+  if (x > -2400) {
+    x += -1200;
+    sliderContainer.setAttribute('style', `left: ${x}px`);
+    console.log(x);
+  } else { return }
+});
