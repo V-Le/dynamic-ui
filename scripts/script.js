88e0f1d99f61933ff9a6841fc1a8ac89eb9b2cc7
Victor L <le.victor@hotmail.com>
2022-07-14 09:09:05 -0500

    added bottom dot navigation

--- scripts/script.js
+++ scripts/script.js
@@ -26,40 +26,49 @@ var imageIndex = 0;
 sliderLeftBtn.addEventListener('click', () => {
   if (imageIndex <= 0 ){
     sliderImageContainer[imageIndex].classList.toggle('show');
+    navDots[imageIndex].classList.toggle('active');
     imageIndex = sliderImageContainer.length -1;
     sliderImageContainer[imageIndex].classList.toggle('show');
+    navDots[imageIndex].classList.toggle('active');
   } else {
     sliderImageContainer[imageIndex].classList.toggle('show');
+    navDots[imageIndex].classList.toggle('active');
     imageIndex -= 1;
     sliderImageContainer[imageIndex].classList.toggle('show');
-
+    navDots[imageIndex].classList.toggle('active');
   }
 })
 
 sliderRightBtn.addEventListener('click', () => {
   if (imageIndex >= (sliderImageContainer.length -1) ){
     sliderImageContainer[imageIndex].classList.toggle('show');
+    navDots[imageIndex].classList.toggle('active');
     imageIndex = 0;
     sliderImageContainer[imageIndex].classList.toggle('show');
+    navDots[imageIndex].classList.toggle('active');
   } else {
     sliderImageContainer[imageIndex].classList.toggle('show');
+    navDots[imageIndex].classList.toggle('active');
     imageIndex += 1;
     sliderImageContainer[imageIndex].classList.toggle('show');
+    navDots[imageIndex].classList.toggle('active');
   }
 })
 
 const navDotsContainer = document.querySelector('.nav__dots__container');
-
 for (let i = 0; i <= sliderImageContainer.length-1; i++) {
   navDotsContainer.innerHTML += '<i class="fa-solid fa-circle"></i>';
 }
 
 const navDots = document.querySelectorAll('.fa-circle');
+navDots[imageIndex].classList.toggle('active');
 for (let x = 0; x <= sliderImageContainer.length-1; x++) {
   navDots[x].addEventListener('click', () => {
     sliderImageContainer[imageIndex].classList.toggle('show');
+    navDots[imageIndex].classList.toggle('active');
     imageIndex = x;
     sliderImageContainer[imageIndex].classList.toggle('show');
+    navDots[imageIndex].classList.toggle('active');
   })
 }
 
